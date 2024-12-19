edfecb0d6fb56a20fbd82dcc16496bbabe39babd
Kevin Coll <kevincollmorales@gmail.com>
2024-12-19 16:36:01 +0100

    nada

--- src/main/java/classes/Personatge.java
+++ src/main/java/classes/Personatge.java
@@ -8,12 +8,60 @@ public class Personatge {
     private int nivel;
     private double experiencia;
 
-    public Personatge(String nombre, String tipo, int fuerza, double experiencia) {
+    public Personatge(String nombre, String tipo) {
         this.nombre = nombre;
         this.tipo = tipo;
-        this.fuerza = fuerza;
+        this.fuerza = 50;
         this.vida = 100;
         this.nivel = 0;
+        this.experiencia = 0;
+    }
+
+    public double getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(double experiencia) {
         this.experiencia = experiencia;
+    }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
     }
 }
