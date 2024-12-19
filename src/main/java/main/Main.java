6e4ec6b26c1ecc5fba20d1f99d52af71919dfed7
Alex Hernandez <ahernandezguerrero582@gmail.com>
2024-12-19 16:06:10 +0100

    Menu inicio NUEVO

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         boolean engine = true;
         while (engine) {
-            System.out.println("1. Crear personage\n2. Modificar personage\n3. Ranking\n4. Eliminar personatge\n5. Mejor personage\n6. JUGAR\n7. POWER OFF");
+            System.out.println("1. Crear personage\n2. Modificar personage\n3. Ranking\n4. Eliminar personatge\n5. Mejor personage\n6. JUGAR\n7. SALIR");
             int opcion = ask.askInt("Dime una opción: ", "Dime una opción correcta: ", 1, 7);
 
         }
