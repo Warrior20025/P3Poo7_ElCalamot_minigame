2c660f292857ae1722e3f1a6b51741eb5d3c96f1
Kevin Coll <kevincollmorales@gmail.com>
2024-12-19 16:00:16 +0100

    Menu

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,7 +2,13 @@ package main;
 
 
 public class Main {
+    private static AskData ask;
     public static void main(String[] args) {
+        boolean engine = true;
+        while (engine) {
+            System.out.println("1. Crear personaje \n2.Modificar personaje \n3.Jugar \n4. POWER OFF");
+            int opcion = ask.askInt("Dime una opción:","Dime una opción correcta",1,4);
+        }
 
     }
 }
