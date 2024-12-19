fed521514a25429a96f98fae39de223f1835d200
Alex Hernandez <ahernandezguerrero582@gmail.com>
2024-12-19 15:28:11 +0100

    getters and setters

--- src/main/java/classes/Personatge.java
+++ src/main/java/classes/Personatge.java
@@ -16,4 +16,52 @@ public class Personatge {
         this.nivel = 0;
         this.experiencia = experiencia;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public double getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(double experiencia) {
+        this.experiencia = experiencia;
+    }
 }
