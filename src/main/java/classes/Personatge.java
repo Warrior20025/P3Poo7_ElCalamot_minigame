63135a19de20869093c933a64111a7467cd0bf7a
Alex Hernandez <ahernandezguerrero582@gmail.com>
2024-12-19 15:23:44 +0100

    inicio Personatge

--- src/main/java/classes/Personatge.java
+++ src/main/java/classes/Personatge.java
@@ -1,5 +1,19 @@
 package classes;
 
 public class Personatge {
-    
+    private String nombre;
+    private String tipo;
+    private int fuerza;
+    private double vida;
+    private int nivel;
+    private double experiencia;
+
+    public Personatge(String nombre, String tipo, int fuerza, double experiencia) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.fuerza = fuerza;
+        this.vida = 100;
+        this.nivel = 0;
+        this.experiencia = experiencia;
+    }
 }
