9a137755fc29b8b592419f4afff6e183528c93fc
Alex Hernandez <ahernandezguerrero582@gmail.com>
2024-12-20 11:46:55 +0100

    final parte 1

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -16,9 +16,10 @@ public class Main {
             int opcion = ask.askInt("Dime una opción: ", "Dime una opción correcta: ", 1, 7);
             switch (opcion) {
                 case 1:
-                    pedirPersonajeMethod();
+                    crearPersonajeMethod();
                     break;
                 case 2:
+
 
             }
 
@@ -30,8 +31,8 @@ public class Main {
 
 
 
-    private static void pedirPersonajeMethod() {
-        String nombre = ask.askString("Dime el nombre del personaje: ");
+    private static void crearPersonajeMethod() {
+        String nombre = ask.askString("Dime el nombre del personaje que quieres crear: ");
         int nombreRepetido = 0;
         for (Personatge i : personajes) {
             if (i.getNombre().equalsIgnoreCase(nombre)) {
@@ -39,9 +40,18 @@ public class Main {
             }
         }
         if (nombreRepetido == 0) {
-            String tipo = ask.askString("Dime que tipo de personaje es " + nombre + ": ");
+            String tipo = "";
+            while (!tipo.equals("guerrero") && !tipo.equals("mago") && !tipo.equals("elfo") && !tipo.equals("enano")){
+                tipo = ask.askString("Dime que tipo de personaje es " + nombre + " (guerrero, mago, elfo, enano): ");
+                tipo = tipo.toLowerCase();
+            }
+
             Personatge nuevoPersonaje = new Personatge(nombre, tipo);
             personajes.add(nuevoPersonaje);
+
+            for (Personatge i : personajes) {
+                System.out.println("nombre: " + i.getNombre() + " tipo: " + i.getTipo());
+            }
 
             System.out.println("Se ha creado un personaje de tipo " + tipo + " con " + nombre + " como nombre.");
         }else { System.out.println("Ya hay un personaje con ese nombre."); }
